c87374fa8930f54e1e1ce637f6364aee11de85cf
Won il Park <wonilpark@Wons-iMac.local>
2022-01-26 11:01:48 +0900

    #5 2.7 Themes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ThemeProvider } from 'styled-components';
+import App from './App';
+
+const darkTheme = {
+  /**
+   * textColor
+   * borderColor
+   * linkColor
+   * linkHoverColor
+   * 등 더 다양하게 지정할 수 있다
+   */
+  textColor: 'whitesmoke',
+  backgroundColor: '#111',
+};
+
+const lightTheme = {
+  textColor: '#111',
+  backgroundColor: 'whitesmoke',
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={lightTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );
