df51fe2c7d501ee173ff466ef29a8e906f064554
Won il Park <wonilpark@Wons-iMac.local>
2022-01-27 14:07:37 +0900

    #17 4.4 Route States

--- coin-tracker/src/routes/Coin.tsx
+++ coin-tracker/src/routes/Coin.tsx
@@ -1,9 +1,46 @@
-import { useParams } from "react-router-dom";
+import { useState } from "react";
+import { useLocation, useParams } from "react-router-dom";
+import styled from "styled-components";
+
+const Container = styled.div`
+  padding: 0px 20px;
+  max-width: 480px;
+  margin: 0 auto;
+`;
+
+const Header = styled.div`
+  height: 15vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.h1`
+  font-size: 48px;
+  color: ${(props) => props.theme.accentColor};
+`;
+
+const Loader = styled.div`
+  text-align: center;
+  display: block;
+`;
+
+interface RouteState {
+  name: string;
+}
 
 const Coin = () => {
+  const [loading, setLoading] = useState(true);
   const { coinId } = useParams();
-
-  return <div>Coin: {coinId}</div>;
+  const { name } = useLocation().state as RouteState;
+  return (
+    <Container>
+      <Header>
+        <Title>{name}</Title>
+      </Header>
+      {loading ? <Loader>Loading...</Loader> : null}
+    </Container>
+  );
 };
 
 export default Coin;
