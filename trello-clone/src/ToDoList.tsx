b196227536f2f24fcae4bfeb1aab38fa8a7d9a02
WONILLISM <wonillism@gmail.com>
2022-02-05 15:12:33 +0900

    #32 5.8 Form Errors

--- trello-clone/src/ToDoList.tsx
+++ trello-clone/src/ToDoList.tsx
@@ -23,33 +23,75 @@ import { useForm } from "react-hook-form";
 //   );
 // };
 
+interface IForm {
+  //필수가 아니라면 항목에 물음표
+  email: string;
+  firstName: string;
+  lastName: string;
+  password1: string;
+  password: string;
+}
+
 const ToDoList = () => {
-  const { register, handleSubmit, formState } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IForm>({
+    defaultValues: {
+      email: "a",
+    },
+  });
   const onValid = (data: any) => {};
-  console.log(formState.errors);
+
   return (
     <div>
       <form
         style={{ display: "flex", flexDirection: "column" }}
         onSubmit={handleSubmit(onValid)}
       >
-        <input {...register("email", { required: true })} placeholder="Email" />
         <input
-          {...register("firstName", { required: true })}
+          {...register("email", {
+            required: "이메일이 필요합니다.",
+            pattern: {
+              value:
+                /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/,
+              message: "이메일 형식이 올바르지 않습니다.",
+            },
+          })}
+          placeholder="Email"
+        />
+        {/* <span>{errors.email.type === "require" ? "이메일이 필요합니다."}</span> */}
+        <span>{errors?.email?.message}</span>
+        <input
+          {...register("firstName", { required: "필수값 입니다." })}
           placeholder="First Name"
         />
+        <span>{errors?.firstName?.message}</span>
         <input
-          {...register("lastName", { required: true, maxLength: 10 })}
+          {...register("lastName", {
+            required: "필수값 입니다.",
+            maxLength: 10,
+          })}
           placeholder="Last Name"
         />
+        <span>{errors?.lastName?.message}</span>
         <input
-          {...register("password", { required: true, minLength: 5 })}
+          {...register("password", {
+            required: "필수값 입니다.",
+            minLength: 5,
+          })}
           placeholder="Password"
         />
+        <span>{errors?.password?.message}</span>
         <input
-          {...register("password1", { required: true, minLength: 5 })}
+          {...register("password1", {
+            required: "필수값 입니다.",
+            minLength: 5,
+          })}
           placeholder="Confirm Password"
         />
+        <span>{errors?.password1?.message}</span>
         <button>Add</button>
       </form>
     </div>
